700d7053dc08bd0822ad09acf0b58a55efb13b82
ductrivan2605 <116768692+ductrivan2605@users.noreply.github.com>
2023-04-24 05:07:15 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/* RMIT University Vietnam
+Course: COSC2430 Web Programming
+Semester: 2023A
+Assessment: Assignment 1
+Author: Van Duc Tri
+ID: s3978223
+*/
+function toggleMobileMenu (menu) {
+    menu.classList.toggle('open');
+    }
